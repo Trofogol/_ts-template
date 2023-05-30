910ecfd68c7b1a23315207df6789a756490564a7
Trofogol <nickb.ufeim@gmail.com>
2023-05-30 10:53:04 +0300

    updated code of main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 // this is sample code to make sure everything is working
 const greetUser = (user: string) => {
-  console.log(`Greetings, ${user}!`);
-  console.log(`Thank you for choosing this repo as your jumpstart kit.`)
-  console.log(`If you are seeing this message, your environment is ready for Typescript development`)
+  console.log(`\nGreetings, ${user}!`);
+  console.log(`\nThank you for choosing this repo as your jumpstart kit.`)
+  console.log(`\nIf you are seeing this message, your environment is ready for Typescript development\n\n`)
 }
 
 greetUser(`user of _ts_basic_template`);
